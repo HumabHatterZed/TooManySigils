6884361a919e8f90b91eefdf824662f11b1a961d
WhistleWind <humanhaterq@gmail.com>
2024-07-12 16:49:37 -0700

    Unimportant internal refactoring because I have personal preferences on how my files are organised and how many unnecessary warnings fill my console

--- obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TooManySigils
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "TooManySigils";
-        public const string PLUGIN_NAME = "TooManySigils";
-        public const string PLUGIN_VERSION = "2.5.0";
-    }
-}
