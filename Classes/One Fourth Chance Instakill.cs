793b13acbce08a461e2d5e324e55d131184b3b29
WhistleWind <humanhaterq@gmail.com>
2024-07-12 16:53:45 -0700

    Changed the trigger to SlotTargetedForAttack since it can bypass the whole attack sequence AND provides more info to work with, also cleaned up the logic

--- Classes/One Fourth Chance Instakill.cs
+++ Classes/One Fourth Chance Instakill.cs
@@ -15,37 +15,42 @@ namespace TooManySigils.Classes
             }
         }
 
-        public override bool RespondsToCardGettingAttacked(PlayableCard source)
+        // i assume with this sigil, you're trying to avoid the base card taking any damage - this will take care of that
+        public override bool RespondsToSlotTargetedForAttack(CardSlot slot, PlayableCard attacker)
         {
-            return base.Card.Health > 0 && !base.Card.Dead && this.Card.AttackedThisTurn;
+            return slot == base.Card.Slot && base.Card.Health > 0 && !base.Card.Dead && base.Card.AttackedThisTurn;
         }
 
-        public override IEnumerator OnTakeDamage(PlayableCard source)
+        public override IEnumerator OnSlotTargetedForAttack(CardSlot slot, PlayableCard attacker)
         {
-            int RandomInt = UnityEngine.Random.Range(1, 5);
-            if (RandomInt == 1)
+            // (0, 4) means the same as (1, 5), i just prefer 0-indexing; feel free to revert back
+            // also i condensed the variable into the if-statement, since you don't use the value anywhere else
+            if (UnityEngine.Random.Range(0, 4) == 0)
             {
                 Singleton<ViewManager>.Instance.SwitchToView(View.Board, false, false);
                 yield return new WaitForSeconds(0.15f);
-                if (source != null)
+                if (attacker != null)
                 {
-                    yield return source.Die(false, null, false);
+                    yield return attacker.Die(false, null, false);
                     Console.WriteLine("Success");
-                } else
+                }
+                else
                 {
-                    Console.WriteLine("Unsuccsessful: Source Null");
+                    Console.WriteLine("Unsuccessful: Card == null");
                 }
                 yield return new WaitForSeconds(0.3f);
-            } else if (RandomInt == 2 || RandomInt == 3 || RandomInt == 4) {
-                Console.WriteLine("Unsuccsessful: Random = 2-4");
             }
-            else
+            else // don't need to check for the other values
+            {
+                Console.WriteLine("Unsuccessful: Random == 1-3"); // also changed the message
+            }
+            /*else // this CANNOT happen, delete this part
             {
                 Console.WriteLine("Unsuccessful: Not In Range");
-            }
+            }*/
             yield return base.LearnAbility(0.1f);
-            yield break;
         }
+
         public static Ability ability;
     }
 }
